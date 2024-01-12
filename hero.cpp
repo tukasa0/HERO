440d7db3bf63c196c54b668d86e6e60d3911b761
sk2k0 <sk2k0@MSI>
2024-01-12 09:20:43 +0900

    コピーコンストラクタ 代入演算子のオーバーロード

--- hero.cpp
+++ hero.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
+#include <algorithm>
+
 using namespace std;
 
 enum Status
@@ -21,13 +23,18 @@ class Enemy
 public:
 	Enemy();
 	~Enemy();
-	void Attack(Hero* hero);              // ダメージの修正
-	int GetStatus(int status);            // 関数名の変更
-	void SetHp(int changeInHp);           // 関数名の変更
-	void ShowName();                       // publicに変更
+	Enemy(const Enemy& other);      //コピーコンストラクタ
+	void operator=(const Enemy& other);     //代入演算子のオーバーロード
+	void Attack(Hero* hero);
+	int GetStatus(const int status) const;
+	void SetHp(const int changeInHp);
+	void ShowName() const;
 
 private:
-	const char* name;
+	void InputName();
+
+private:
+	char* name;
 	int hp;
 	int atk;
 	int def;
@@ -38,12 +45,14 @@ class Hero
 public:
 	Hero();
 	~Hero();
-	void Attack(Enemy* enemy);           // ダメージの修正
+	Hero(const Hero& other);            // コピーコンストラクタ
+	void operator=(const Hero& other);  // 代入演算子のオーバーロード
+	void Attack(Enemy* enemy);
 	void Heal();
-	int GetStatus(int status);           // 関数名の変更
-	void SetHp(int changeInHp);          // 関数名の変更
+	int GetStatus(const int status) const;
+	void SetHp(const int changeInHp);
 	int InputKey();
-	void ShowName();                      // publicに変更
+	void ShowName() const;
 
 
 private:
@@ -61,33 +70,62 @@ private:
 };
 
 
-
-//敵の定義
 Enemy::Enemy()
 {
-	name = "enemy";
-	hp = 10;
+	InputName();
+
+	cout << "HPを設定してください > " << flush;
+	cin >> hp;
+
 	atk = 5;
 	def = 2;
 }
 
+//コピーコンストラクタ
+Enemy::Enemy(const Enemy& other)
+{
+	name = new char[strlen(other.name) + 1];
+	strcpy(name, other.name);
+
+	hp = other.hp;
+	atk = other.atk;
+	def = other.def;
+
+	//copy(other.name, other.name + def, name);
+
+	//cout << "コピーコンストラクタが呼ばれました。" << endl;
+}
+
+//代入演算子のオーバーロード
+void Enemy::operator=(const Enemy& other)
+{
+	char* opName = new char[strlen(other.name) + 1];
+	strcpy(opName, other.name);
+	delete[] name;
+	name = opName;
+
+	hp = other.hp;
+	atk = other.atk;
+	def = other.def;
+}
+
 void Enemy::Attack(Hero* hero)
 {
-	int damage = hero->GetStatus(Def) - atk;        // ダメージ変数の追加
+	int damage = hero->GetStatus(Def) - atk;
 
-	if (damage > 0)                                 // 修正
+	if (damage > 0)
 	{
-		damage = 0;                                 // damageがマイナスの値でなければ0を入れる
+		damage = 0;
 	}
 
 	cout << name << "の攻撃" << endl;
-	hero->ShowName();	                        // ShowNameに修正
+	hero->ShowName();
 	cout << "に " << abs(damage) << " のダメージ" << endl;
 	
 	hero->SetHp(damage);
 }
 
-int Enemy::GetStatus(int status)
+int Enemy::GetStatus(const int status) const
 {
 	switch (status)
 	{
@@ -102,7 +140,7 @@ int Enemy::GetStatus(int status)
 	case Def:
 		return def;
 		break;
-		                                   // caseNameの削除
+
 	default:
 		break;
 	}
@@ -113,24 +151,49 @@ void Enemy::SetHp(int changeInHp)
 	hp += changeInHp;
 }
 
-void Enemy::ShowName()
+void Enemy::InputName()
+{
+	if (name != NULL)
+	{
+		delete[] name;
+		name = NULL;
+	}
+
+	const int MAX_NAME = 30;
+	char inputName[MAX_NAME + 1];
+
+	cout << "敵の名前を入力してください > " << flush;
+	cin >> inputName;
+
+	int size = strlen(inputName);
+	name = new char[size + 1];
+
+	strcpy(name, inputName);
+
+	cout << "敵の名前は " << name << " です。" << endl;
+}
+
+void Enemy::ShowName() const
 {
 	cout << name;
 }
 
 Enemy::~Enemy()
 {
-	
+	if (name != NULL)
+	{
+		delete[] name;
+		name = NULL;
+	}
+
+	//cout << "デストラクタが呼ばれました。" << endl;
 };
 
 
 
-//プレイヤーの定義
 Hero::Hero()
 {
 	InputName();
-	//InputName();
-	//InputName();
 
 	cout << "HPを設定してください > " << flush;
 	cin >> hp;
@@ -139,22 +202,49 @@ Hero::Hero()
 	def = 2;
 };
 
+//コピーコンストラクタ
+Hero::Hero(const Hero& other)
+{
+	name = new char[strlen(other.name) + 1];
+	strcpy(name, other.name);
+
+	hp = other.hp;
+	atk = other.atk;
+	def = other.def;
+
+	//copy(other.name, other.name + def, name);
+
+	//cout << "コピーコンストラクタが呼ばれました。" << endl;
+}
+
+//代入演算子のオーバーロード
+void Hero::operator=(const Hero& other)
+{
+	char* opName = new char[strlen(other.name) + 1];
+	strcpy(opName, other.name);
+	delete[] name;
+	name = opName;
+
+	hp = other.hp;
+	atk = other.atk;
+	def = other.def;
+}
+
 void Hero::Attack(Enemy* enemy)
 {
-	int damage = enemy->GetStatus(Def) - atk;          // ダメージ変数の追加
+	int damage = enemy->GetStatus(Def) - atk;
 
-	if (damage > 0)                                    // 修正
+	if (damage > 0)
 	{
-		damage = 0;                                    // damageがマイナスの値でなければ0を入れる
+		damage = 0;
 	}
 
 	cout << name << "の攻撃" << endl;
-	enemy->ShowName();                                 // ShowNameに修正
+	enemy->ShowName();
 	cout << "に " << abs(damage) << " のダメージ" << endl;
 
 	enemy->SetHp(damage); 
 }
-
 
 void Hero::Heal()
 {
@@ -164,7 +254,7 @@ void Hero::Heal()
 	hp += heal;
 }
 
-int Hero::GetStatus(int status)
+int Hero::GetStatus(const int status) const
 {
 	switch (status)
 	{
@@ -179,7 +269,7 @@ int Hero::GetStatus(int status)
 	case Def:
 		return def;
 		break;
-		                                    // caseNameの削除
+
 	default:
 		break;
 	}
@@ -190,14 +280,14 @@ void Hero::SetHp(const int changeInHp)
 	hp += changeInHp;
 }
 
-void Hero::ShowName()
+void Hero::ShowName() const
 {
 	cout << name;
 }
 
 void Hero::InputName()
 {
-	if (name != NULL)                             // new二重防止コードの追加
+	if (name != NULL)
 	{
 		delete[] name;
 		name = NULL;
@@ -206,13 +296,13 @@ void Hero::InputName()
 	const int MAX_NAME = 30;
 	char inputName[MAX_NAME + 1];
 	
-	cout << "名前を入力してください > " << flush;
+	cout << "あなたの名前を入力してください > " << flush;
 	cin >> inputName;
 
-	int size = strlen(inputName);                // strlenの使用に変更
+	int size = strlen(inputName);
 	name = new char[size + 1];
 
-	strcpy(name, inputName);                     // strcpyの使用に変更
+	strcpy(name, inputName);
 
 	cout << "あなたの名前は " << name << " です。" << endl;
 }
@@ -232,9 +322,12 @@ Hero::~Hero()
 {
 	if (name != NULL)
 	{
-		delete[] name;      // 解放はnewでとった値を解放するだけでポインタをdeleteするわけではないから先に開放を行う。
-		name = NULL;        // 修正
+		delete[] name;
+		name = NULL;
 	}
+
+	//cout << "デストラクタが呼ばれました。" << endl;
+
 };
 
 
@@ -248,9 +341,9 @@ void Battle(Hero* hero)
 	while (true)
 	{
 		cout << endl;
-		hero->ShowName();     // ShowNameに修正
+		hero->ShowName();
 		cout << " HP " << heroNowHp << endl;
-		enemy.ShowName();     // ShowNameに修正
+		enemy.ShowName();
 		cout << " HP " << enemyNowHp << endl;
 
 		int action = hero->InputKey();
@@ -282,10 +375,30 @@ void Battle(Hero* hero)
 	}
 }
 
+void showStatus(Hero hero, Enemy enemy);
+
 int main()
 {
 	Hero hero;
+	Hero otherHero;
+	Enemy enemy;
+	Enemy otherEnemy;
 
-	Battle(&hero);
+	//Battle(&hero);
 
+	showStatus(hero, enemy); // 代入前
+	hero = otherHero;
+	enemy = otherEnemy;
+	showStatus(hero, enemy); // 代入後
+}
+
+void showStatus(const Hero const hero, const Enemy const enemy)
+{
+	cout << "あなたのパラメータ" << endl;
+	hero.ShowName();
+	printf(" HP:%d ATK:%d DEF:%d\n", hero.GetStatus(Hp), hero.GetStatus(Atk), hero.GetStatus(Def));
+
+	cout << "敵のパラメータ" << endl;
+	enemy.ShowName();
+	printf(" HP:%d ATK:%d DEF:%d\n", enemy.GetStatus(Hp), enemy.GetStatus(Atk), enemy.GetStatus(Def));
 }
